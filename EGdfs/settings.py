630400f3e5446872181a938d93edf1f3d30a553c
PretzelOptician <alekschultz1@gmail.com>
2022-10-10 21:51:42 -0400

    EGdfs app setup and basic home page

--- EGdfs/settings.py
+++ EGdfs/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'EGdfs.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'EGdfs_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'EGdfs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/EGdfs/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,6 +125,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
---
+++ EGdfs_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EgdfsAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'EGdfs_app'
